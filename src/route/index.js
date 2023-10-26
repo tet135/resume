76daa76d9d7db40b5a0e1d21a1f62857cf38bfd3
your_name <tsapko.tatyana@gmail.com>
2023-10-26 17:34:04 +0300

    bootstrap task3.1

--- src/route/index.js
+++ src/route/index.js
@@ -971,10 +971,6 @@ router.get('/car', function (req, res) {
     },
   })
 })
-=======
-  //=====================================  ↑↑ сюди вводимо JSON дані
-})
-
 
 router.get('/js', function (req, res) {
   // res.render генерує нам HTML сторінку
@@ -1062,7 +1058,6 @@ router.get('/js', function (req, res) {
     res.render('task22', {
       layout: 'basic',
 
-
       page_header: {
         title: 'InnovaMind',
         description: 'Welcome to our wonderful community',
@@ -1116,7 +1111,7 @@ router.get('/js', function (req, res) {
     })
     //                  ↑↑ сюди вводимо JSON дані
   })
-=======
+
 router.get('/task21', function (req, res) {
   // res.render генерує нам HTML сторінку
 
@@ -1186,7 +1181,291 @@ router.get('/task21', function (req, res) {
   //                  ↑↑ сюди вводимо JSON дані
 })
 
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/task31', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('task31', {
+    layout: 'basic',
+
+    navigation: {
+      links: [
+        {
+          text: 'Home',
+          href: 'https://github.com/',
+        },
+        {
+          text: 'Contacts',
+          href: 'https://www.google.com/',
+        },
+        {
+          text: 'Help',
+          href: 'https://www.youtube.com/',
+        },
+      ],
+      button: {
+        text: 'Lets start',
+        href: 'https://nodejs.org/en/',
+      },
+    },
+    header: {
+      title: 'Welcome to Our Website',
+      description:
+        'We offer innovative solutions to help grow your business. Our team of experts is committed to providing tailored strategies that deliver the best results.',
+      button: {
+        text: 'Learn More',
+        link: 'https://www.youtube.com/',
+      },
+    },
+    city: {
+      title: 'City list',
+      cards: [
+        {
+          picture: {
+            src: 'https://picsum.photos/400/200',
+            alt: 'New York',
+          },
+          title: 'New York City',
+          description:
+            'New York City comprises 5 boroughs sitting where the Hudson River meets the Atlantic Ocean. At its core is Manhattan, a densely populated borough that’s among the world’s major commercial, financial and cultural centers. Its iconic sites include skyscrapers such as the Empire State Building and sprawling Central Park.',
+          button: {
+            text: 'Read',
+            href: 'https://github.com/',
+          },
+        },
+        {
+          picture: {
+            src: 'https://picsum.photos/400/200',
+            alt: 'Paris',
+          },
+          title: 'Paris',
+          description:
+            "Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy, and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine. Beyond such landmarks as the Eiffel Tower and the 12th-century, Gothic Notre-Dame cathedral, the city is known for its cafe culture, haute couture fashion houses, and designer boutiques along the Rue du Faubourg Saint-Honoré.",
+          button: {
+            text: 'Read',
+            href: 'https://github.com/',
+          },
+        },
+        {
+          picture: {
+            src: 'https://picsum.photos/400/200',
+            alt: 'Tokyo',
+          },
+          title: 'Tokyo',
+          description:
+            "Tokyo, Japan’s bustling capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic temples. The opulent Meiji Shinto Shrine is known for its towering gate and surrounding woods. The Imperial Palace sits amid large public gardens. The city's many museums offer exhibits ranging from classical art (in the Tokyo National Museum) to a reconstructed kabuki theater (in the Edo-Tokyo Museum).",
+          button: {
+            text: 'Read',
+            href: 'https://github.com/',
+          },
+        },
+        {
+          picture: {
+            src: 'https://picsum.photos/400/200',
+            alt: 'Sydney',
+          },
+          title: 'Sydney',
+          description:
+            "Sydney, capital of New South Wales and one of Australia's largest cities, is best known for its harborfront Opera House, with a distinctive sail-like design. Massive Darling Harbour and the smaller Circular Quay port are hubs of waterside life, with the arched Harbour Bridge and esteemed Royal Botanic Garden nearby. Sydney Tower’s outdoor platform, the Skywalk, offers 360-degree views of the city and suburbs.",
+          button: {
+            text: 'Read',
+            href: 'https://github.com/',
+          },
+        },
+      ],
+    },
+    winner: {
+      title: 'Our winners',
+      table: {
+        head: ['#', 'name', 'surname', 'nickname'],
+        user: [
+          {
+            place: 1,
+            name: 'Ada',
+            surname: 'Lovelace',
+            nickname: 'The Enchantress of Numbers',
+          },
+          {
+            place: 2,
+            name: 'Grace',
+            surname: 'Hopper',
+            nickname: 'Amazing Grace',
+          },
+          {
+            place: 3,
+            name: 'Katherine',
+            surname: 'Johnson',
+            nickname: 'The Human Computer',
+          },
+        ],
+      },
+    },
+    gallery: {
+      title: 'Picture history',
+      cards: [
+        {
+          name: 'The Mona Lisa',
+          items: [
+            'Painting',
+            'Oil on poplar panel',
+            '1503-1519',
+          ],
+        },
+        {
+          name: 'The Persistence of Memory',
+          items: ['Painting', 'Oil on canvas', '1931'],
+        },
+        {
+          name: 'The Starry Night',
+          items: ['Painting', 'Oil on canvas', '1889'],
+        },
+      ],
+    },
+    relaxion: {
+      title: 'Relax',
+      cards: [
+        {
+          header: 'Meditation',
+          title: 'The Benefits of Meditation',
+          description:
+            'Meditation can help reduce stress, anxiety, and depression, and improve overall well-being. Studies have also shown that regular meditation practice can enhance cognitive function and even boost the immune system.',
+        },
+        {
+          header: 'Hydration',
+          title: 'The Importance of Hydration',
+          description:
+            'Drinking enough water is essential for good health. Dehydration can lead to a range of symptoms, including fatigue, headaches, and constipation. It can also increase the risk of kidney stones and other health problems.',
+        },
+        {
+          header: 'Yoga',
+          title: 'The Benefits of Yoga',
+          description:
+            'Yoga can improve flexibility, strength, and balance, and help reduce stress and anxiety. It has also been shown to improve heart health and promote a sense of well-being.',
+        },
+        {
+          header: 'Sunscreen',
+          title: 'The Importance of Sunscreen',
+          description:
+            "Protecting your skin from the sun's harmful rays is crucial for reducing the risk of skin cancer and premature aging. Experts recommend using a broad-spectrum sunscreen with an SPF of at least 30 and reapplying every two hours.",
+        },
+      ],
+    },
+    rest: {
+      cards: [
+        {
+          header: 'Cooking',
+          title: 'The Joy of Cooking',
+          description:
+            'Cooking is a creative outlet that can bring people together over delicious food.',
+          button: 'Learn More',
+          footer: 'Photo by Brooke Lark on Unsplash',
+        },
+        {
+          header: 'Travel',
+          title: 'The Benefits of Traveling',
+          description:
+            'Traveling broadens your perspective, provides new experiences, and creates lasting memories.',
+          button: 'Explore',
+          footer: 'Photo by Sime Basioli on Unsplash',
+        },
+      ],
+    },
+    proposition: {
+      title: 'Proposition',
+      card: [
+        {
+          header: 'Our Approach',
+          title: 'Innovative Solutions for Your Business',
+          description:
+            'We create effective solutions that cater to your business needs.',
+        },
+        {
+          header: 'Our Expertise',
+          title: 'Trusted and Experienced Professionals',
+          description:
+            'Our team of experts delivers tailored strategies for best results.',
+        },
+      ],
+    },
+    footer: {
+      title: 'Navigation',
+      navigation: [
+        {
+          text: 'Home',
+          href: 'https://github.com/',
+        },
+        {
+          text: 'Contacts',
+          href: 'https://www.google.com/',
+        },
+        {
+          text: 'Help',
+          href: 'https://www.youtube.com/',
+        },
+      ],
+      socialMedia: [
+        {
+          name: 'Facebook',
+          link: 'https://www.facebook.com/',
+        },
+        {
+          name: 'Twitter',
+          link: 'https://twitter.com/',
+        },
+        {
+          name: 'Instagram',
+          link: 'https://www.instagram.com/',
+        },
+      ],
+      partners: [
+        {
+          name: 'Acme Inc.',
+          link: 'https://www.acme.com/',
+        },
+        {
+          name: 'Globex Corporation',
+          link: 'https://www.globex.com/',
+        },
+        {
+          name: 'Stark Industries',
+          link: 'https://www.starkindustries.com/',
+        },
+      ],
+    },
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
 
 // ================================================================
+
+router.get('/bootstrap', function (req, res) {
+  res.render('bootstrap', {
+    layout: 'basic',
+    page: {
+      title: 'Резюме|bootstrap',
+    },
+    header,
+
+    main: {
+      sammary: {
+        title: 'Summary',
+        text: `Open-minded for new technologies, with 1 years of experience in development. Whenever I start to work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.`,
+      },
+      experience: {
+        title: 'Other experience',
+        text: `Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics ( tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.`,
+      },
+    },
+
+    footer,
+  })
+})
+
+// ====
 // Підключаємо роутер до бек-енду
 module.exports = router
